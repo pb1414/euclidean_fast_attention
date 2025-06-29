a5dfcc5bca0b01c05417d930a049b51d017dd5b8
Thorben Frank <thorbenjan.frank@gmail.com>
2025-06-29 13:44:52 +0200

    add config for NaCl

---
+++ euclidean_fast_attention/configs/trainer/4ghdnnp_NaCl.py
@@ -0,0 +1,29 @@
+"""The default configuration for the experiments."""
+
+import ml_collections
+
+from ase import units
+from ml_collections import config_dict
+
+
+def get_config():
+    """Get the default hyperparameter configuration."""
+    config = ml_collections.ConfigDict()
+    
+    config.datafile = config_dict.placeholder(str)
+    config.num_train = 4000
+    config.num_valid = 500
+    config.split_seed = 0
+    config.model_seed = 0
+    config.max_num_nodes = 8 * 17 + 1
+    config.max_num_edges = 8 * 17 * 12 + 1
+    config.max_num_graphs = 8 + 1
+    config.num_epochs = None
+    config.num_train_steps = 500_000
+    config.save_interval_steps = 5000
+    config.log_loss_every_steps = 500
+    config.energy_unit = units.eV
+    config.length_unit = units.Angstrom
+    config.pbc_bool = False
+
+    return config
