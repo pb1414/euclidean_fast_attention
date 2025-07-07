9a4e8ee30be2841fab314f8bc1bad74235336661
Thorben Frank <thorbenjan.frank@gmail.com>
2025-07-07 14:41:12 +0200

    add AT_AT and AT_AT_CG_CG to config

---
+++ euclidean_fast_attention/configs/trainer/md22_AT_AT_CG_CG.py
@@ -0,0 +1,33 @@
+"""The default configuration for the experiments."""
+
+import ml_collections
+
+from ase import units
+from ml_collections import config_dict
+
+
+def get_config():
+    """Get the default hyperparameter configuration."""
+    config = ml_collections.ConfigDict()
+    
+    config.datafile = config_dict.placeholder(str)
+    config.num_train = 2_000
+    config.num_valid = 500
+    config.split_seed = 0
+    config.model_seed = 0
+    config.max_num_nodes = 8 * 118 + 1
+    config.max_num_edges = 8 * 118 * 18 + 1  # works for cutoff 4.0
+    config.max_num_graphs = 8 + 1
+    config.num_epochs = None
+    config.num_train_steps = 1_000_000
+    config.save_interval_steps = 5000
+    config.log_loss_every_steps = 500
+    config.energy_unit = units.eV
+    config.length_unit = units.Angstrom
+    config.log_loss_every_steps = 50
+    config.pbc_bool = False
+    config.auto_eval = True
+    config.subtract_energy_mean = True
+    config.neighbor_list_cutoff = None
+
+    return config
