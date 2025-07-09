a7ea0a408a60eb07f298765fa65ad84fb661447c
Thorben Frank <thorbenjan.frank@gmail.com>
2025-07-09 17:19:48 +0200

    add config for NaCl sphere experiments

---
+++ euclidean_fast_attention/configs/trainer/NaCl_sphere.py
@@ -0,0 +1,42 @@
+"""The default configuration for the experiments."""
+
+import ml_collections
+
+from ase import units
+from ml_collections import config_dict
+
+
+def get_config(num_atoms: int):
+    """Get the default hyperparameter configuration."""
+    config = ml_collections.ConfigDict()
+    
+    config.datafile = config_dict.placeholder(str)
+    config.num_train = 4000
+    config.num_valid = 500
+    config.split_seed = 0
+    config.model_seed = 0
+    config.max_num_nodes = 16 * num_atoms + 1
+    config.max_num_edges = 16 * num_atoms * avg_num_neighbors_lookup[num_atoms] + 1
+    config.max_num_graphs = 16 + 1
+    config.num_epochs = None
+    config.num_train_steps = 500_000
+    config.save_interval_steps = 5_000
+    config.log_loss_every_steps = 500
+    config.energy_unit = units.eV
+    config.length_unit = units.Angstrom
+    config.pbc_bool = False
+    config.auto_eval = True
+    config.subtract_energy_mean = True
+    config.neighbor_list_cutoff = config_dict.placeholder(float)
+
+    return config
+
+
+# assumes a model cutoff of 5.0 Angstrom
+avg_num_neighbors_lookup = {
+    16: 3,
+    32: 3,
+    64: 3,
+    128: 3,
+    256: 5,
+}
