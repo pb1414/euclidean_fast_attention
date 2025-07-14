533c6b19689c8574a0ecc7e5da7e41f6373190cd
Thorben Frank <thorbenjan.frank@gmail.com>
2025-07-14 13:01:14 +0200

    add configs for remaining MD22 molecules

---
+++ euclidean_fast_attention/configs/trainer/md22_nanotube.py
@@ -0,0 +1,34 @@
+"""The default configuration for the experiments."""
+
+import ml_collections
+
+from ase import units
+from ml_collections import config_dict
+
+
+def get_config():
+    """Get the default hyperparameter configuration."""
+    config = ml_collections.ConfigDict()
+    
+    config.datafile = config_dict.placeholder(str)
+    config.num_train = 800
+    config.num_valid = 500
+    config.split_seed = 0
+    config.model_seed = 0
+    config.max_num_nodes = 2 * 370 + 1
+    config.max_num_edges = 2 * 370 * 25 + 1  # works for cutoff 4.0
+    config.max_num_graphs = 2 + 1
+    config.num_epochs = None
+    config.num_train_steps = 1_000_000
+    config.save_interval_steps = 5000
+    config.log_loss_every_steps = 50
+    config.energy_unit = units.eV
+    config.length_unit = units.Angstrom
+    config.pbc_bool = False
+    config.auto_eval = True
+    config.subtract_energy_mean = True
+    config.energy_weight = 0.001
+    config.forces_weight = 0.999
+    config.neighbor_list_cutoff = config_dict.placeholder(float)
+
+    return config
