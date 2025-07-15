d143f91b610077ac2d466139447b6d02c35661aa
Thorben Frank <thorbenjan.frank@gmail.com>
2025-07-15 15:17:46 +0200

    adapt config for MD17

--- euclidean_fast_attention/configs/trainer/md17.py
+++ euclidean_fast_attention/configs/trainer/md17.py
@@ -21,10 +21,9 @@ def get_config(split: str):
     config.num_epochs = None
     config.num_train_steps = 1_000_000
     config.save_interval_steps = 5000
-    config.log_loss_every_steps = 500
+    config.log_loss_every_steps = 50
     config.energy_unit = units.eV
     config.length_unit = units.Angstrom
-    config.log_loss_every_steps = 50
     config.pbc_bool = False
     config.auto_eval = True
     config.subtract_energy_mean = True
